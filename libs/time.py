89c4d2c393908edceabaed272d2c9e00c30b7f5a
Andrey Miroshnichenko <zentavr@gmail.com>
2024-12-06 14:48:50 +0100

    Fixing an issue when with ROS7 the timestamps were not parsed in the right way

--- libs/time.py
+++ libs/time.py
@@ -2,37 +2,49 @@
 
 import re
 
-
 def time_convert(t):
     """
     Converts Mikrotik's time string to seconds
     :param t: Mikrotik's time string
     :return: seconds
     """
-    timeRegEx = re.split('(\d+\w)', t, flags=re.UNICODE)
+    print("Received time: {time}".format(time=t))
+
+    # Validate the format first
+    if not re.fullmatch(r"(\d+[a-zA-Z]+)+", t):
+        raise Exception("Invalid format: {time}".format(time=t))
+
+    time_regex = re.findall(r"(?P<count>\d+)(?P<time_mult>[a-zA-Z]+)", t, flags=re.UNICODE)
     seconds = 0
 
-    for time_item in timeRegEx:
-        match = re.match(r"(?P<count>\d+)(?P<time_mult>\w)", time_item)
-        if match:
-            if match.group('time_mult') == 's':
-                # print "{count} sec".format(count=bgp_match.group('count'))
-                seconds += int(match.group('count'))
-            elif match.group('time_mult') == 'm':
-                # print "{count} min".format(count=bgp_match.group('count'))
-                seconds += int(match.group('count')) * 60
-            elif match.group('time_mult') == 'h':
-                # print "{count} hours".format(count=bgp_match.group('count'))
-                seconds += int(match.group('count')) * 60 * 60
-            elif match.group('time_mult') == 'd':
-                # print "{count} days".format(count=bgp_match.group('count'))
-                seconds += int(match.group('count')) * 60 * 60 * 24
-            elif match.group('time_mult') == 'w':
-                # print "{count} weeks".format(count=bgp_match.group('count'))
-                seconds += int(match.group('count')) * 60 * 60 * 24 * 7
-            else:
-                raise Exception("Have no idea how to deal with {time_mult} value".format(
-                    time_mult=match.group('time_mult')
-                ))
+    for count, time_mult in time_regex:
+        print("Time item: {count}{time_mult}".format(
+            count=count,
+            time_mult=time_mult)
+        )
+        count = int(count)
+
+        if time_mult == 'ms':
+            print("{count} msec".format(count=count))
+            seconds += 0
+        elif time_mult == 's':
+            print("{count} sec".format(count=count))
+            seconds += count
+        elif time_mult == 'm':
+            print("{count} min".format(count=count))
+            seconds += count * 60
+        elif time_mult == 'h':
+            print("{count} hours".format(count=count))
+            seconds += count * 60 * 60
+        elif time_mult == 'd':
+            print("{count} days".format(count=count))
+            seconds += count * 60 * 60 * 24
+        elif time_mult == 'w':
+            print("{count} weeks".format(count=count))
+            seconds += count * 60 * 60 * 24 * 7
+        else:
+            raise Exception("Have no idea how to deal with {time_mult} value".format(
+                time_mult=time_mult
+            ))
 
     return seconds
